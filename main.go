773d45e5c2a26d8388d491a103513dbd1e4b614d
Jude Harding <jharding@genesisdla.com>
2022-03-29 16:47:41 -0400

    wip loops

--- main.go
+++ main.go
@@ -1,83 +1,67 @@
 package main
 
-// the fmt package is used for input/output functionality
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main()   {
+func main() {
 
-	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	// array that can hold 50 items
-	var bookings [50]string
+	conferenceName := "Go Conference"
+	bookings := []string{}
 
-	// a flexible array is called a slice -- use a slice for better memory management
-	var bookingslice [] string
-
-	// different syntax for same thing above but ONLY works on VARS
-	//  conferenceName  := "Go Conference"
-	//  remainingTickets  := 50 // can use on int but NOT uint
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
 
-	// finding out what data type the vars below are 
-	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	// PLAIN FOR LOOP
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
+		// asking for user input
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
 
-	fmt.Println("Welcome to our ", conferenceName, " booking application")
-	// fmt.Println("We have a total of ", conferenceTickets, " and ", remainingTickets, " are still available")
-	// Printf allows you to print the %VARIABLES in different formats - see documentation 
-	// below uses place holders with printF and above is the standard way with printLN 
-	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
 
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
 
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		//CHECKING TO SEE IF USER TIX IS GREATER THAN REMAINING TIX
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets remaining so you cannot book %v tickets", remainingTickets, userTickets)
+			continue
+		}
 
-	fmt.Println(remainingTickets)
-	fmt.Println(&remainingTickets)
+		// book ticket in system
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
+		// FOR EACH LOOP - print only first names
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names %v\n", firstNames)
 
-	// we cannot assign "tom" to the UN but we can POINT (&) to the memory address where the user enters the name AND
-	// the program will wait until you enter a name in terminal 
-	// userName = "Tom"
-	fmt.Println("Enter your first name:  ")
-	fmt.Scan(&firstName)
-  // SCAN pauses the program and waits on user entry
-	// Pointers & are needed when waiting on data to be entered
+		if remainingTickets == 0 {
+			// END PROGRAM
+			fmt.Println("Our conference is sold out.  Come back next year!")
+			break
 
-	fmt.Println("Enter your last name:  ")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email:  ")
-	fmt.Scan(&email)
-	fmt.Println("How many tickets do you want:  ")
-	fmt.Scan(&userTickets)
-
-  remainingTickets = remainingTickets - userTickets
-  bookings[0] = firstName  + " " + lastName
-	// no need for an index when using SLICE instead of ARRAY 
-	bookingslice = append(bookingslice, firstName + " " + lastName)
-
-	fmt.Println("-----")
-	fmt.Printf("Thank you %v %v for purchasing %v tickets.  You will receive a confirmation email at %v . \n", firstName, lastName, userTickets, email)
-
-	fmt.Println("-----")
-	fmt.Printf("%v tickets remain open for t %v \n", remainingTickets, conferenceName)
-
-	fmt.Println("-----")
-	fmt.Printf("The WHOLE array%v \n", bookings)
-	fmt.Printf("The first value in the array%v \n", bookings[0])
-	fmt.Printf("The array type is %T \n", bookings)
-	fmt.Printf("The array length is %v \n", len(bookings))
-
-	fmt.Println("-----")
-	fmt.Printf("The WHOLE SLICE ARRAY is %v \n", bookingslice)
-	fmt.Printf("The first value in the SLICE%v \n", bookingslice[0])
-	fmt.Printf("The SLICE type is %T \n", bookingslice)
-	fmt.Printf("The SLICE length is %v \n", len(bookingslice))
+		}
+	}
 }
 
