9681706e309656be3458aadb0793ffa91fffd38b
Jude Harding <jharding@genesisdla.com>
2022-03-30 16:25:10 -0400

    wip golang if-else

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
 
-	// PLAIN FOR LOOP
+		// PLAIN FOR LOOP
 	for {
 		var firstName string
 		var lastName string
@@ -35,33 +35,62 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scanln(&userTickets)
 
-		//CHECKING TO SEE IF USER TIX IS GREATER THAN REMAINING TIX
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining so you cannot book %v tickets", remainingTickets, userTickets)
+		// validate user input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			// book ticket in system
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			// print only first names
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names %v\n", firstNames)
+
+			// exit application if no tickets are left
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("firt name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
 			continue
-		}
-
-		// book ticket in system
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-		// FOR EACH LOOP - print only first names
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("The first names %v\n", firstNames)
-
-		if remainingTickets == 0 {
-			// END PROGRAM
-			fmt.Println("Our conference is sold out.  Come back next year!")
-			break
-
 		}
 	}
 }
 
+// 	// Switch statement example
+// 	city := "London"
+
+// 	switch city {
+// 	case "New York":
+// 		// booking for New York conference
+// 	case "Singapore", "Hong Kong":
+// 		// booking for Singapore & Hong Kong conference
+// 	case "London", "Berlin":
+// 		// booking for London & Berlin conference
+// 	case "Mexico City":
+// 		// booking for Mexico City conference
+// 	default:
+// 		fmt.Print("No valid city selected")
+// 	}
+// }
