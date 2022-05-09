6d330d5a8b256d23b3dc46b8b399d59002054b51
Jude Harding <jharding@genesisdla.com>
2022-05-09 15:32:27 -0400

    working to 2.16

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 // DEFINING PACKAGE LEVEL VARS are available to ALL functions on this page
-const conferenceTickets uint = 50
+const conferenceTickets int = 50
 var remainingTickets uint = 50
 var conferenceName string = "Go Conference" // OR conferenceName := "Go Conference"
+// **********var conferenceName = "Go Conference"
 var bookings = []string{} // no defined length makes this a SLICE
+
 
 func main() {
 
@@ -20,42 +22,48 @@ func main() {
 	for {
 	
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+
+
+		// // validate user input func
+		// validateUserInput(firstName, lastName, email, userTickets)
 		
-
-
-		// validate user input func
-		validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
-		
-
 		if isValidName && isValidEmail && isValidTicketNumber {
 
-			bookTicket(remainingTickets, userTickets, firstName, lastName, email)
+			bookTicket(userTickets, firstName, lastName, email)
+
+			firstNames := printFirstNames()
+			fmt.Printf("The first names %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				break
 
 
-			// CONLOG NOTES
-			fmt.Println("\n-------------------------")
-			// printing WHOLE array 
-			fmt.Printf("The WHOLE slice is : %v\n", bookings)
-			fmt.Printf("The FIRST value in the array is : %v\n", bookings[0])
-			fmt.Printf("The slice TYPE is : %T\n", bookings)
-			fmt.Printf("The slice LENGTH is : %v\n\n", len(bookings))
 
-			fmt.Println("-------------------------")
+			// // CONLOG NOTES
+			// fmt.Println("\n-------------------------")
+			// // printing WHOLE array 
+			// fmt.Printf("The WHOLE slice is : %v\n", bookings)
+			// fmt.Printf("The FIRST value in the array is : %v\n", bookings[0])
+			// fmt.Printf("The slice TYPE is : %T\n", bookings)
+			// fmt.Printf("The slice LENGTH is : %v\n\n", len(bookings))
+
+			// fmt.Println("-------------------------")
 		
 
 			// call function print first names
-			getFirstNames()
+			// firstNames()
 			// receiving a value from the function
-			firstNames := getFirstNames(bookings)
-			fmt.Printf("\n\nThe first names are:  %v\n\n", firstNames)
+			// firstNames := printFirstNames(bookings)
+			// fmt.Printf("\n\nThe first names are:  %v\n\n", firstNames)
 
 			// LOGIC for tickets remaing 
 			// exit application if no tickets are left
-			if remainingTickets == 0 {
-				// END FOR LOOP and program
-				fmt.Println("Our conference is booked out. Come back next year.")
-				break
+			// if remainingTickets == 0 {
+			// 	// END FOR LOOP and program
+			// 	fmt.Println("Our conference is booked out. Come back next year.")
+			// 	break
 			}
 		} else {
 			// LOGIC for user data entry
@@ -74,43 +82,29 @@ func main() {
 }
 
 
-
-func greetUsers(){  
-	fmt.Printf("\n\nWELCOME NEWBIE to the %v booking app\n", conferenceName)
-	fmt.Printf("We have total of  %v tickets and %v are available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-	fmt.Println("")
-}
-
-func getFirstNames() []string {
+func printFirstNames() []string {
 	// create a SLICE of first names
 	firstNames := []string{}
 	// print only first names FOR EACH LOOP
-	for _, booking := range bookings {
+	for _, booking := range bookings {	
 		var names = strings.Fields(booking)
 		firstNames = append(firstNames, names[0])
 	}
+
 	// fmt.Printf("\n\nThe first names are:  %v\n\n", firstNames)
 	// RETURNING A VALUE also has to be stated at the top of this func between the () and {} and you return the TYPE
 	return firstNames
 }
 
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint )(bool, bool, bool ){
-		isValidName  := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail  := strings.Contains(email, "@")
-		isValidTicketNumber  := userTickets > 0 && userTickets <= remainingTickets
-		// can return MULTIPLE values from a function  instead of 1
-		return isValidName, isValidEmail, isValidTicketNumber
-}
-
-func getUserInput()(string, string, string, uint){
+func getUserInput() (string, string, string, uint) {	
 		// asking for user input
+
 		var firstName string
 		var lastName string
 		var email string
 		var userTickets uint
-
+		
 		fmt.Println("*****  NEW RECORD  *****")
 		fmt.Println("\n\nEnter Your First Name: ")
 		fmt.Scanln(&firstName)
@@ -127,14 +121,28 @@ func getUserInput()(string, string, string, uint){
 		return firstName, lastName, email, userTickets
 }
 
-func bookTicket(remainingTickets uint, userTickets uint, bookings []string, firstName string, lastName, string, email string, conferenceName string  ){
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	// can return MULTIPLE values from a function  instead of 1
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func greetUsers() {
+		fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
+		fmt.Println("")
+	}
+
+func bookTicket(userTickets uint, firstName string, lastName string,  email string ){
 	// LOGIC for book ticket in system
 	remainingTickets = remainingTickets - userTickets
 
 	// putting items into the array called bookings
 	bookings = append(bookings, firstName+" "+lastName)
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n\n", remainingTickets, conferenceName)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 	fmt.Println("*****  END OF RECORD  *****")
 }
 
